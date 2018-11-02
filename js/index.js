ccd6b5e6dcec16b2f287fbeec146d738f0bb30e5
yourshero <592131412@qq.com>
2018-11-02 11:30:17 +0800

    2018年11月2日11:29:54

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-($(function(){
-    console.log(1);
-    $('.dropdown3').hide();
-    $('#dropdown3-wrap').mouseover(function(){
-        $('.dropdown3').show();
+($(function () {
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        /*window.location.href="你的手机版地址";*/
+        $('.dropdown3').hide();
+        $('#dropdown3-wrap').on('tap',function(){
+            $('.dropdown3').show();
+        })
+    } else {
+        /*window.location.href="你的电脑版地址";    */
+        $('.dropdown3').hide();
+        $('#dropdown3-wrap').mouseover(function () {
+            $('.dropdown3').show();
+        })
+        $('.dropdown3').hover(function () {
+            $('.dropdown3').show();
+        }, function () {
+            $('.dropdown3').hide(500);
+        })
     }
-    )
-    $('.dropdown3').hover(function(){
-        $('.dropdown3').show();
-    },function(){
-        $('.dropdown3').hide(500);
-    }
-        )
 }))
