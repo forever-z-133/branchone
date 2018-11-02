d6d34d5d32c658ceef4b1ab4007f38c96467a0ae
yourshero <592131412@qq.com>
2018-11-02 14:03:30 +0800

    2018年11月2日14:03:01

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
 ($(function () {
+    $("#dropdown3-wrap").on("click",function (e) {
+        e.stopPropagation();
+        //取消Bootstrap的dropdown-menu点击默认关闭事件
+    })
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
         /*window.location.href="你的手机版地址";*/
         $('.dropdown3').hide();
-        $('#dropdown3-wrap').on('tap',function(){
-            $('.dropdown3').show();
+        $('#dropdown3-wrap').on('click',function(){
+            console.log($('#dropdown3-wrap').attr('flag'));
+            if($('#dropdown3-wrap').attr('flag')=='off'){
+                $('.dropdown3').show();
+                $('#dropdown3-wrap').attr('flag','on');
+            }else{
+                $('.dropdown3').hide(500);
+                $('#dropdown3-wrap').attr('flag','off');
+            }
         })
     } else {
-        /*window.location.href="你的电脑版地址";    */
         $('.dropdown3').hide();
+        
         $('#dropdown3-wrap').mouseover(function () {
             $('.dropdown3').show();
         })
@@ -16,5 +27,12 @@
         }, function () {
             $('.dropdown3').hide(500);
         })
-    }
+        }
+
+        /*window.location.href="你的电脑版地址";    */
+       
+        // $("#dropdown3-wrap").die().live("click",function(){
+        //     console.log(1);
+        // });
+
 }))
